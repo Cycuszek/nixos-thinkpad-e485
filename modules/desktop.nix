ba2f95cf7698f91dce3a826cda70b9751ff10a1e
Cycuszek <87500705+Cycuszek@users.noreply.github.com>
2026-04-03 12:22:17 +0200

    Replace noto-fonts-emoji with noto-fonts-color-emoji

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -22,7 +22,7 @@
       nerd-fonts.hack
       noto-fonts
       noto-fonts-cjk-sans
-      noto-fonts-emoji
+      noto-fonts-color-emoji
     ];
     fontconfig.defaultFonts = {
       monospace = [ "JetBrainsMono Nerd Font" ];
@@ -36,3 +36,4 @@
   # -------------------------------------------------------
   xdg.portal.enable = true;
 }
+
